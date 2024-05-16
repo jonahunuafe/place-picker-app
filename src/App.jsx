502d57da40856d1679be6d458169b9194b79e565
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-16 13:38:42 +0100

    implement functionality to fetch available places

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
+import { useState, useEffect } from "react";
 import Places from "./components/Places";
-
-
+import { AVAILABLE_PLACES } from './data.js';
+import { sortPlacesByDistance } from "./loc.js";
 
 
 export default function App() {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES, 
+        position.coords.latitude, 
+        position.coords.longitude
+      );
+
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
+
   return (
     <div>
-      <Places />
+      <Places
+          title="Available Places"
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
+        />
     </div>
   )
 }
