9e12dcd5aa61448d9329fff617c90ceaca3de041
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-16 13:56:25 +0100

    implement functionality to pick places from available places using useState and localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import { AVAILABLE_PLACES } from './data.js';
 import { sortPlacesByDistance } from "./loc.js";
 
 
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) => 
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 export default function App() {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -19,13 +25,39 @@ export default function App() {
     });
   }, []);
 
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if(storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces", 
+        JSON.stringify([id, ...storedIds])
+      );
+    }
+  }
+
   return (
-    <div>
-      <Places
+    <>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={'Select the places you would like to visit below.'}
+          places={pickedPlaces}
+        />
+        <Places
           title="Available Places"
           places={availablePlaces}
           fallbackText="Sorting places by distance..."
+          onSelectPlace={handleSelectPlace}
         />
-    </div>
+      </main>
+    </>
   )
 }
